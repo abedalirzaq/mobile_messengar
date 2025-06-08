93e24f4389a2100a3be60f60abc92956d48d3686
abed alirzaq matary <abedalirzaqmatary12@gmail.com>
2025-06-08 04:02:13 +0300

    start Home Style

---
+++ lib/app/modules/Home/Widget/AppBarHome.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppBarHomeScreen extends StatelessWidget {
+  const AppBarHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
